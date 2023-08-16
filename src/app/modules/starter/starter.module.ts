bbc41d3f0f8d387ace21ca30f83b147b82a497ac
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-16 14:41:03 +0530

    Updated Contene

--- src/app/modules/starter/starter.module.ts
+++ src/app/modules/starter/starter.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { StarterRoutingModule } from './starter-routing.module';
 import { StarterComponent } from './starter.component';
 import { StarterHeaderComponent } from './component/starter-header/starter-header.component';
+import { StarterFooterComponent } from './component/starter-footer/starter-footer.component';
+import { StarterMainSliderComponent } from './component/starter-main-slider/starter-main-slider.component';
+import { StarterMainServicesComponent } from './component/starter-main-services/starter-main-services.component';
 
 
 @NgModule({
   declarations: [
     StarterComponent,
-    StarterHeaderComponent
+    StarterHeaderComponent,
+    StarterFooterComponent,
+    StarterMainSliderComponent,
+    StarterMainServicesComponent
   ],
   imports: [
     CommonModule,
