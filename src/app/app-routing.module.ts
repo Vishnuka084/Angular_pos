2ca84e178674c00bed3d419146c9c057bd8d29d6
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-16 14:04:54 +0530

    Updated Contene

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundPageComponent} from "./component/not-found-page/not-found-page.component";
 
 const routes: Routes = [
   { path:'',redirectTo:'starter',pathMatch:'full'},
   { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
-  { path: 'starter', loadChildren: () => import('./modules/starter/starter.module').then(m => m.StarterModule) }
+  { path: 'starter', loadChildren: () => import('./modules/starter/starter.module').then(m => m.StarterModule) },
+  { path:'**',component:NotFoundPageComponent}
+
 ];
 
 @NgModule({
