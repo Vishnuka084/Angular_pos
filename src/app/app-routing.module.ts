dbee72c367228f12360aee0314ab7ba3bcb10ecf
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-16 13:28:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
