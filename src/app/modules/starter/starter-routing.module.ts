09242a838d0573dc93867a9eed53095453a913a1
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-16 15:38:32 +0530

    Updated Contene

--- src/app/modules/starter/starter-routing.module.ts
+++ src/app/modules/starter/starter-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StarterComponent } from './starter.component';
 import {StarterMainPricingComponent} from "./component/starter-main-pricing/starter-main-pricing.component";
+import {StarterHomeContextComponent} from "./component/starter-home-context/starter-home-context.component";
 
 const routes: Routes = [
   { path: '', component: StarterComponent , children: [
+      { path:'',redirectTo:'/starter/home'},
+      { path:'home',component:StarterHomeContextComponent},
       { path:'pricing',component:StarterMainPricingComponent}
     ]},
 
