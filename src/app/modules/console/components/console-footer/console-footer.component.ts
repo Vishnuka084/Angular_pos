2fdcf5f5bc0f28e4ef1d4b2b9d9f6dcc424148ee
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-16 23:32:51 +0530

    create console footer and header

---
+++ src/app/modules/console/components/console-footer/console-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-console-footer',
+  templateUrl: './console-footer.component.html',
+  styleUrls: ['./console-footer.component.scss']
+})
+export class ConsoleFooterComponent {
+
+}
--- src/app/modules/console/console.module.ts
+++ src/app/modules/console/console.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ConsoleRoutingModule } from './console-routing.module';
 import { ConsoleComponent } from './console.component';
+import { ConsoleHeaderComponent } from './components/console-header/console-header.component';
+import { ConsoleFooterComponent } from './components/console-footer/console-footer.component';
 
 
 @NgModule({
   declarations: [
-    ConsoleComponent
+    ConsoleComponent,
+    ConsoleHeaderComponent,
+    ConsoleFooterComponent
   ],
   imports: [
     CommonModule,
