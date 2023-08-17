43c19cb623028c1fe77f088c7e59e67d3c96b3d4
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-17 17:50:47 +0530

    Updated Contene

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SecurityRoutingModule } from './security-routing.module';
 import { SecurityComponent } from './security.component';
+import { LoginComponent } from './component/login/login.component';
 
 
 @NgModule({
   declarations: [
-    SecurityComponent
+    SecurityComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
